a29ecdcb574995ec8ca96bced1b7b1404c263865
sivanmiroglu <sivanmiroglu854@gmail.com>
2022-12-28 21:37:24 +0300

    Java

--- src/Java101Patikasi/manav.java
+++ src/Java101Patikasi/manav.java
@@ -2,6 +2,18 @@ package Java101Patikasi;
 import java.util.Scanner;
 public class manav {
     public static void main(String[] args) {
+
+        /*Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine
+        göre toplam tutarını ekrana yazdıran programı yazın.
+
+        Meyveler ve KG Fiyatları
+
+        Armut : 2,14 TL
+        Elma : 3,67 TL
+        Domates : 1,11 TL
+        Muz: 0,95 TL
+        Patlıcan : 5,00 TL*/
+        
         Double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, toplam;
         int patlican = 5;
 
