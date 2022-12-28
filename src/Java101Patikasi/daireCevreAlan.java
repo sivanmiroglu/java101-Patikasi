c756a897cb79d67ca1c4d107455103e7651310ad
sivanmiroglu <sivanmiroglu854@gmail.com>
2022-12-28 20:04:25 +0300

    Java

--- src/Java101Patikasi/daireCevreAlan.java
+++ src/Java101Patikasi/daireCevreAlan.java
@@ -1,2 +1,25 @@
-package Java101Patikasi;public class daireCevreAlan {
+package Java101Patikasi;
+import java.util.Scanner;
+public class daireCevreAlan {
+    public static void main(String[] args) {
+       /* Ödev
+        Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+
+        𝜋 sayısını = 3.14 alınız.
+
+         Formül : (𝜋 * (r*r) * 𝛼) / 360 */
+
+        int r;
+        double pi =3.14;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Dairenin Yarı Çapını Giriniz : ");
+        r =inp.nextInt();
+        double alan=pi * r * r;
+        double cevre=2 * pi * r;
+
+        System.out.println("Dairenin alanı :"+alan);
+        System.out.println("Cevrenin alanı : "+cevre);
+    }
 }
--- src/Java101Patikasi/taksimetre.java
+++ src/Java101Patikasi/taksimetre.java
@@ -1,2 +1,24 @@
-package Java101Patikasi;public class taksimetre {
+package Java101Patikasi;
+import  java.util.Scanner;
+public class taksimetre {
+  public static void main(String[] args) {
+        //Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
+
+        //Taksimetre KM başına 2.20 TL tutmaktadır.
+        //Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+        //Taksimetre açılış ücreti 10 TL'dir.
+
+        int km;
+        double perKm= 2.20, total, startPrice = 10;
+
+        Scanner mesafe =new Scanner(System.in);
+        System.out.print("Mesafeyi Km türünden giriniz : ");
+        km = mesafe.nextInt();
+
+        total= (km*perKm);
+        total +=startPrice;
+
+        total=(total <20)? 20 : total;
+        System.out.println("Toplam Tutar :" +total);
+    }
 }
