582c438476e0a44d8fbf7d2e46ce310d952d1be4
sivanmiroglu <sivanmiroglu854@gmail.com>
2023-01-17 20:17:41 +0300

    Java

---
+++ src/Java101Metot/Polindrom.java
@@ -0,0 +1,26 @@
+package Java101Metot;
+
+public class Polindrom {
+    /*Palindromik sayı, iki taraftan okunduğu zaman okunuş yönüyle aynı olan sayılardır.
+
+    Örnek: 1, 4, 8, 99, 101, 363, 4004, 9889....*/
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastDigit;
+        while (temp != 0) {
+            lastDigit = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+            return true;
+        } else
+            return false;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(101));
+    }
+
+}
